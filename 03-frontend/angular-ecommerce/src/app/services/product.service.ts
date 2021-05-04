ce35fe35c12a77b395854736262bba6d62492c82
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-05-03 20:53:34 -0400

    added pagination

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -30,6 +30,16 @@ export class ProductService {
     );
   }
 
+  getProductListPaginate(thePage: number,
+                          thePageSize: number,
+                          theCategoryId: number): Observable<GetResponseProducts> {
+    
+    // need to build URL based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                        + `&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   searchProducts(theKeyword: string): Observable<Product[]> {
 
     // need to build URL based on keyword
@@ -56,6 +66,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
