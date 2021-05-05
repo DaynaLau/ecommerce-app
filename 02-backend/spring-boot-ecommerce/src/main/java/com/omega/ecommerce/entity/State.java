9efdcf4a515fc52cb72841457dbeaf7df9b70288
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-05-05 18:08:25 -0400

    country and state classes

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/omega/ecommerce/entity/State.java
@@ -0,0 +1,23 @@
+package com.omega.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="country")
+    private Country country;
+
+}
