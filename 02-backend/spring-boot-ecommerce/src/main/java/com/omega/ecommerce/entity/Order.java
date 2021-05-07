bc6c7cc01101f3ef3ba5ce922f652d830f453645
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-05-07 17:20:02 -0400

    mapping checkout info to db

--- 02-backend/spring-boot-ecommerce/src/main/java/com/omega/ecommerce/entity/Order.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/omega/ecommerce/entity/Order.java
@@ -2,10 +2,14 @@ package com.omega.ecommerce.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "orders")
@@ -31,11 +35,36 @@ public class Order {
     private String status;
 
     @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
 
     @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 
-    @Column(name = "customer")
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
     private Customer customer;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
+    private Address shippingAddress;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
+    private Address billingAddress;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    public void add(OrderItem item) {
+        if (item != null) {
+            if (orderItems == null) {
+                orderItems = new HashSet<>();
+            }
+
+            orderItems.add(item);
+            item.setOrder(this);
+        }
+    }
 }
