b29859ad18af76d3bfc1527ab629711910a122cb
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-05-13 17:21:41 -0400

    adding security config

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/omega/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.omega.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // add CORS filters
+        http.cors();
+
+    }
+}
